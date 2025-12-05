5e59cee314a7a8d98db3d91054a0535f3920b5a6
Lirum86 <lirum.shop@gmail.com>
2025-12-05 22:57:07 +0100

    Add new bait types with attributes
    Added various bait types with attributes such as Icon, LureSpeed, Luck, GenerelLuck, Resilience, Rarity, Limited status, and Mutation details.

---
+++ Baits.lua
@@ -0,0 +1,788 @@
+         ["Cookie Crumble"] = {
+		Icon = "rbxassetid://98621322518786";
+		LureSpeed = 35;
+		Luck = 320;
+		GenerelLuck = 190;
+		Resilience = 15;
+		Rarity = "Limited";
+		Limited = true;
+		Mutation = "Merry";
+		MutationChance = {1, 100, 10};
+	};
+	["Fruitcake Flakes"] = {
+		Icon = "rbxassetid://84117453444161";
+		LureSpeed = 15;
+		Luck = 135;
+		GenerelLuck = 180;
+		Resilience = 50;
+		Rarity = "Limited";
+		Limited = true;
+		Mutation = "Merry";
+		MutationChance = {1, 100, 10};
+	};
+	["Frostnova Bait"] = {
+		Icon = "rbxassetid://124837407305551";
+		LureSpeed = 35;
+		Luck = 120;
+		GenerelLuck = 150;
+		Resilience = 50;
+		Rarity = "Limited";
+		Limited = true;
+		Mutation = "Frostnova";
+		MutationChance = {1, 100, 30};
+		ProgressBoost = 50;
+	};
+	["Pumpkin Pieces"] = {
+		Icon = "rbxassetid://114504690206576";
+		LureSpeed = 5;
+		Luck = 120;
+		GenerelLuck = 150;
+		Resilience = 50;
+		Rarity = "Limited";
+		Limited = true;
+		Mutation = "Gravy";
+		MutationChance = {1, 100, 20};
+		ProgressBoost = 10;
+	};
+	["Cranberry Cluster"] = {
+		Icon = "rbxassetid://122403049190756";
+		LureSpeed = 60;
+		Luck = 70;
+		GenerelLuck = 90;
+		Resilience = -5;
+		Rarity = "Limited";
+		Limited = true;
+		Mutation = "Gravy";
+		MutationChance = {1, 100, 20};
+		ProgressBoost = 25;
+	};
+	["Gourd Bites"] = {
+		Icon = "rbxassetid://103488070952855";
+		LureSpeed = 10;
+		Luck = 100;
+		GenerelLuck = 80;
+		Resilience = 35;
+		Rarity = "Limited";
+		Limited = true;
+		Mutation = "Gravy";
+		MutationChance = {1, 100, 20};
+	};
+	["Starlight Worm"] = {
+		Icon = "rbxassetid://130760356284577";
+		LureSpeed = 30;
+		Luck = 300;
+		GenerelLuck = 100;
+		Resilience = 15;
+		Rarity = "Secret";
+		Limited = true;
+		Mutation = "Nova";
+		MutationChance = {1, 100, 30};
+		ProgressBoost = 20;
+	};
+	["Candy Corn"] = {
+		Icon = "rbxassetid://113512853309050";
+		LureSpeed = 80;
+		Luck = 150;
+		GenerelLuck = 90;
+		Resilience = -40;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	["Gummy Fish"] = {
+		Icon = "rbxassetid://89838811379302";
+		LureSpeed = 30;
+		Luck = 130;
+		GenerelLuck = 85;
+		Resilience = -5;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	Gobstopper = {
+		Icon = "rbxassetid://71105831108185";
+		LureSpeed = -50;
+		Luck = 500;
+		GenerelLuck = 500;
+		Resilience = 100;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	["Hallow-Pop"] = {
+		Icon = "rbxassetid://92776826028846";
+		LureSpeed = 30;
+		Luck = 155;
+		GenerelLuck = 65;
+		Resilience = 10;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	["Sour Gummy Worm"] = {
+		Icon = "rbxassetid://107879301420393";
+		LureSpeed = 40;
+		Luck = 195;
+		GenerelLuck = 85;
+		Resilience = -20;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	Taco = {
+		Icon = "rbxassetid://87539476182048";
+		Mutation = "Spicy";
+		LureSpeed = 100;
+		Luck = 100;
+		GenerelLuck = 100;
+		Resilience = -20;
+		Rarity = "Special";
+		XpBoost = 1;
+		Limited = true;
+	};
+	["Golden Coin"] = {
+		Icon = "rbxassetid://117895646767362";
+		Mutation = "Radiant";
+		LureSpeed = 0;
+		Luck = 1000;
+		GenerelLuck = 1000;
+		Resilience = 0;
+		Rarity = "Special";
+		XpBoost = 1;
+		Limited = true;
+	};
+	Singularity = {
+		Icon = "rbxassetid://106266362085397";
+		Mutation = "Surreal";
+		LureSpeed = -20;
+		Luck = 0;
+		GenerelLuck = 0;
+		Resilience = 150;
+		Rarity = "Special";
+		XpBoost = 1;
+		Limited = true;
+	};
+	UFO = {
+		Icon = "rbxassetid://133811860015401";
+		Mutation = "Alien";
+		LureSpeed = 77;
+		Luck = 110;
+		GenerelLuck = 177;
+		Resilience = 70;
+		Rarity = "Special";
+		XpBoost = 1;
+		Limited = true;
+	};
+	Ant = {
+		Icon = "rbxassetid://71835820456161";
+		LureSpeed = -10;
+		Luck = 40;
+		GenerelLuck = 5;
+		Resilience = -15;
+		Rarity = "Trash";
+		Limited = true;
+		ProgressBoost = 5;
+	};
+	["Beetle Grub"] = {
+		Icon = "rbxassetid://105174229397460";
+		LureSpeed = -5;
+		Luck = 70;
+		GenerelLuck = 15;
+		Resilience = 5;
+		Rarity = "Uncommon";
+		Limited = true;
+		SlashChance = 3;
+		SlashColor = Color3.fromRGB(160, 82, 45);
+		SlashDamage = 2.5;
+	};
+	Centipede = {
+		Icon = "rbxassetid://105989049110593";
+		LureSpeed = 20;
+		Luck = 200;
+		GenerelLuck = 60;
+		Resilience = -10;
+		Rarity = "Legendary";
+		Limited = true;
+		XpBoost = 1;
+	};
+	Cricket = {
+		Icon = "rbxassetid://138108366316913";
+		LureSpeed = 10;
+		Luck = 90;
+		GenerelLuck = 25;
+		Resilience = 0;
+		Rarity = "Unusual";
+		Limited = true;
+		XpBoost = 0.5;
+	};
+	Dragonfly = {
+		Icon = "rbxassetid://117519458070322";
+		LureSpeed = 25;
+		Luck = 160;
+		GenerelLuck = 50;
+		Resilience = -5;
+		Rarity = "Exotic";
+		Limited = true;
+		ProgressBoost = 20;
+	};
+	Earthworm = {
+		Icon = "rbxassetid://76933739253900";
+		LureSpeed = -15;
+		Luck = 60;
+		GenerelLuck = 10;
+		Resilience = 20;
+		Rarity = "Common";
+		Limited = true;
+		Mutation = "Dirty";
+	};
+	Glowworm = {
+		Icon = "rbxassetid://86659602459947";
+		LureSpeed = 15;
+		Luck = 300;
+		GenerelLuck = 100;
+		Resilience = 5;
+		Rarity = "Secret";
+		Limited = true;
+		Mutation = "Glowy";
+		ProgressBoost = 50;
+	};
+	Snail = {
+		Icon = "rbxassetid://118347840774742";
+		LureSpeed = -20;
+		Luck = 80;
+		GenerelLuck = -30;
+		Resilience = 30;
+		Rarity = "Rare";
+		Limited = true;
+		Control = 0.1;
+	};
+	["Stag Beetle"] = {
+		Icon = "rbxassetid://107975714738894";
+		LureSpeed = 35;
+		Luck = 120;
+		GenerelLuck = 35;
+		Resilience = -5;
+		Rarity = "Mythical";
+		Limited = true;
+		SlashChance = 10;
+		SlashColor = Color3.fromRGB(160, 82, 45);
+		SlashDamage = 2.5;
+	};
+	Star = {
+		Icon = "rbxassetid://85895036109272";
+		Mutation = "Galaxy";
+		LureSpeed = 80;
+		Luck = 120;
+		GenerelLuck = 120;
+		Resilience = -10;
+		Rarity = "Special";
+		XpBoost = 1;
+		Limited = true;
+	};
+	["Tryhard Worm"] = {
+		Icon = "rbxassetid://134690438417592";
+		LureSpeed = 45;
+		Luck = 50000;
+		GenerelLuck = 0;
+		Resilience = 65;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	Meteor = {
+		Icon = "rbxassetid://78140806286048";
+		LureSpeed = 10;
+		Luck = 55;
+		GenerelLuck = 65;
+		Resilience = 100;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	Umbrella = {
+		Icon = "rbxassetid://71775608851867";
+		LureSpeed = 50;
+		Luck = 125;
+		GenerelLuck = 50;
+		Resilience = 30;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	["Beach Ball"] = {
+		Icon = "rbxassetid://71291086018125";
+		LureSpeed = 150;
+		Luck = 5;
+		GenerelLuck = 5;
+		Resilience = 0;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	["Beached Pearl"] = {
+		Icon = "rbxassetid://72150831526088";
+		LureSpeed = 95;
+		Luck = 250;
+		GenerelLuck = 300;
+		Resilience = 30;
+		Rarity = "Limited";
+		Limited = true;
+	};
+	Crawlurion = {
+		Icon = "rbxassetid://84860326921778";
+		LureSpeed = 10;
+		Luck = 25;
+		GenerelLuck = 30;
+		Resilience = 10;
+		Rarity = "Secret";
+		Limited = true;
+	};
+	Moonhaze = {
+		Icon = "rbxassetid://122138201465584";
+		LureSpeed = 60;
+		Luck = 225;
+		GenerelLuck = 230;
+		Resilience = 5;
+		Rarity = "Secret";
+		Limited = true;
+	};
+	Astrolure = {
+		Icon = "rbxassetid://134629744900115";
+		Mutation = "Galactic";
+		LureSpeed = 90;
+		Luck = 175;
+		GenerelLuck = 60;
+		Resilience = 60;
+		Rarity = "Secret";
+		XpBoost = 1;
+		Limited = true;
+	};
+	Nullbit = {
+		Icon = "rbxassetid://80815966615997";
+		Mutation = "Nullified";
+		LureSpeed = 60;
+		Luck = 120;
+		GenerelLuck = 120;
+		Resilience = -20;
+		Rarity = "Secret";
+		XpBoost = 1;
+		Limited = true;
+	};
+	Snare = {
+		Icon = "rbxassetid://131502303758768";
+		LureSpeed = 80;
+		Luck = 125;
+		GenerelLuck = -10;
+		Resilience = -20;
+		Rarity = "Secret";
+		XpBoost = 1;
+	};
+	["Golden Worm"] = {
+		Icon = "rbxassetid://92588934474259";
+		LureSpeed = 20;
+		Luck = 200;
+		GenerelLuck = 100;
+		Resilience = 45;
+		Rarity = "Exotic";
+	};
+	["Glass Beetle"] = {
+		Icon = "rbxassetid://108650710805383";
+		LureSpeed = 30;
+		Luck = 125;
+		GenerelLuck = -80;
+		Resilience = 30;
+		Rarity = "Rare";
+	};
+	["Neuro Slug"] = {
+		Icon = "rbxassetid://98378589712685";
+		LureSpeed = -20;
+		Luck = 90;
+		GenerelLuck = 100;
+		Resilience = 60;
+		Rarity = "Legendary";
+	};
+	["Nightmare Larva"] = {
+		Icon = "rbxassetid://139717203005579";
+		LureSpeed = 25;
+		Luck = 120;
+		GenerelLuck = 35;
+		Resilience = 25;
+		Rarity = "Mythical";
+	};
+	["Phantom Leech"] = {
+		Icon = "rbxassetid://123505973177484";
+		LureSpeed = 10;
+		Luck = 55;
+		GenerelLuck = 30;
+		Resilience = 25;
+		Rarity = "Rare";
+	};
+	["Toxic Jelly Core"] = {
+		Icon = "rbxassetid://103314073936320";
+		LureSpeed = -5;
+		Luck = 180;
+		GenerelLuck = 90;
+		Resilience = 35;
+		Rarity = "Legendary";
+	};
+	Lucky = {
+		Icon = "rbxassetid://83124206397968";
+		Mutation = "Blarney";
+		MutationChance = {1, 100, 50};
+		LureSpeed = 20;
+		Luck = 200;
+		GenerelLuck = 150;
+		Resilience = 15;
+		Rarity = "Mythical";
+		Limited = true;
+	};
+	["Chocolate Fish"] = {
+		Icon = "rbxassetid://122149688355947";
+		Mutation = "Chocolate";
+		LureSpeed = 20;
+		Luck = 200;
+		GenerelLuck = 150;
+		Resilience = 15;
+		Rarity = "Mythical";
+		Limited = true;
+	};
+	Bagel = {
+		Icon = "rbxassetid://108678002533415";
+		LureSpeed = 0;
+		Luck = 25;
+		GenerelLuck = 0;
+		Resilience = 15;
+		Rarity = "Common";
+	};
+	Garbage = {
+		Icon = "rbxassetid://134913713768472";
+		LureSpeed = -5;
+		Luck = 0;
+		GenerelLuck = -250;
+		Resilience = 50;
+		Rarity = "Common";
+	};
+	Worm = {
+		Icon = "rbxassetid://133997443467885";
+		LureSpeed = 15;
+		Luck = 25;
+		GenerelLuck = 0;
+		Resilience = 0;
+		Rarity = "Common";
+	};
+	Insect = {
+		Icon = "rbxassetid://82228384077490";
+		LureSpeed = 5;
+		Luck = 35;
+		GenerelLuck = 0;
+		Resilience = 0;
+		Rarity = "Common";
+	};
+	Maggot = {
+		Icon = "rbxassetid://77502257509358";
+		LureSpeed = -10;
+		Luck = 50;
+		GenerelLuck = 35;
+		Resilience = 0;
+		Rarity = "Uncommon";
+	};
+	Squid = {
+		Icon = "rbxassetid://132083884054870";
+		LureSpeed = -25;
+		Luck = 55;
+		GenerelLuck = 45;
+		Resilience = 0;
+		Rarity = "Unusual";
+	};
+	Seaweed = {
+		Icon = "rbxassetid://78676762283786";
+		LureSpeed = 20;
+		Luck = 35;
+		GenerelLuck = 0;
+		Resilience = 10;
+		Rarity = "Unusual";
+	};
+	Coral = {
+		Icon = "rbxassetid://88295442439044";
+		LureSpeed = 20;
+		Luck = 50;
+		GenerelLuck = 0;
+		Resilience = 20;
+		Rarity = "Unusual";
+	};
+	["Deep Coral"] = {
+		Icon = "rbxassetid://71969712298871";
+		LureSpeed = 0;
+		Luck = 50;
+		GenerelLuck = -10;
+		Resilience = 50;
+		Rarity = "Legendary";
+	};
+	Flakes = {
+		Icon = "rbxassetid://89483992302592";
+		LureSpeed = 10;
+		Luck = 55;
+		GenerelLuck = 0;
+		Resilience = -3;
+		Rarity = "Common";
+	};
+	Shrimp = {
+		Icon = "rbxassetid://129458957024181";
+		LureSpeed = 0;
+		Luck = 45;
+		GenerelLuck = 25;
+		Resilience = -5;
+		Rarity = "Uncommon";
+	};
+	Magnet = {
+		Icon = "rbxassetid://127635918006645";
+		LureSpeed = 0;
+		Luck = 200;
+		GenerelLuck = 0;
+		Resilience = 0;
+		Rarity = "Unusual";
+	};
+	["Truffle Worm"] = {
+		Icon = "rbxassetid://93019675927218";
+		LureSpeed = -10;
+		Luck = 300;
+		GenerelLuck = 0;
+		Resilience = 0;
+		Rarity = "Legendary";
+	};
+	Minnow = {
+		Icon = "rbxassetid://127324539778135";
+		LureSpeed = 0;
+		Luck = 65;
+		GenerelLuck = 0;
+		Resilience = -10;
+		Rarity = "Unusual";
+	};
+	Coal = {
+		Icon = "rbxassetid://128813695014519";
+		LureSpeed = 0;
+		Luck = 45;
+		GenerelLuck = 0;
+		Resilience = -10;
+		Rarity = "Rare";
+	};
+	["Rapid Catcher"] = {
+		Icon = "rbxassetid://123038194837270";
+		LureSpeed = 35;
+		Luck = 0;
+		GenerelLuck = 0;
+		Resilience = -15;
+		Rarity = "Rare";
+	};
+	["Instant Catcher"] = {
+		Icon = "rbxassetid://105765233417193";
+		LureSpeed = 65;
+		Luck = 0;
+		GenerelLuck = -20;
+		Resilience = -15;
+		Rarity = "Legendary";
+	};
+	["Super Flakes"] = {
+		Icon = "rbxassetid://105233590701549";
+		LureSpeed = 0;
+		Luck = 100;
+		GenerelLuck = 70;
+		Resilience = -15;
+		Rarity = "Rare";
+	};
+	["Night Shrimp"] = {
+		Icon = "rbxassetid://93937013256967";
+		LureSpeed = 15;
+		Luck = 100;
+		GenerelLuck = 90;
+		Resilience = 0;
+		Rarity = "Legendary";
+	};
+	["Fish Head"] = {
+		Icon = "rbxassetid://82318594895677";
+		LureSpeed = 10;
+		Luck = 150;
+		GenerelLuck = 0;
+		Resilience = -10;
+		Rarity = "Legendary";
+	};
+	["Weird Algae"] = {
+		Icon = "rbxassetid://100508147066133";
+		LureSpeed = -35;
+		Luck = 100;
+		GenerelLuck = 200;
+		Resilience = 0;
+		Rarity = "Legendary";
+	};
+	["Shark Head"] = {
+		Icon = "rbxassetid://134025031777629";
+		LureSpeed = -5;
+		Luck = 225;
+		GenerelLuck = 30;
+		Resilience = 10;
+		Rarity = "Mythical";
+	};
+	["Aurora Bait"] = {
+		Icon = "rbxassetid://85141818620239";
+		LureSpeed = -5;
+		Luck = 100;
+		GenerelLuck = 30;
+		Resilience = 10;
+		Rarity = "Mythical";
+		Limited = true;
+		Mutation = "Aurora";
+		MutationChance = {1, 100, 30};
+	};
+	["Peppermint Worm"] = {
+		DontGiveToANewAccount = true;
+		Icon = "rbxassetid://93911511219039";
+		LureSpeed = -5;
+		Luck = 50;
+		GenerelLuck = 30;
+		Resilience = 20;
+		Rarity = "Unusual";
+		Limited = true;
+	};
+	["Holly Berry"] = {
+		DontGiveToANewAccount = true;
+		Icon = "rbxassetid://78831863135519";
+		LureSpeed = -5;
+		Luck = 80;
+		GenerelLuck = 30;
+		Resilience = 10;
+		Rarity = "Legendary";
+		Limited = true;
+		Mutation = "Jolly";
+		MutationChance = {1, 100, 20};
+	};
+	["Hangman's Hook"] = {
+		Icon = "rbxassetid://129988917083814";
+		LureSpeed = -5;
+		Luck = 150;
+		GenerelLuck = 35;
+		Resilience = 20;
+		ProgressBoost = 45;
+		Rarity = "Mythical";
+	};
+	["Kraken Tentacle"] = {
+		Icon = "rbxassetid://81433219929632";
+		LureSpeed = 35;
+		Luck = 100;
+		GenerelLuck = 35;
+		Resilience = 15;
+		Rarity = "Legendary";
+	};
+	["Golden Tentacle"] = {
+		Icon = "rbxassetid://83405723534819";
+		LureSpeed = 50;
+		Luck = 150;
+		GenerelLuck = 40;
+		Resilience = 20;
+		Rarity = "Legendary";
+		Limited = true;
+	};
+	Lushrooms = {
+		LureSpeed = 10;
+		Luck = 50;
+		GenerelLuck = 20;
+		Resilience = 10;
+		Rarity = "Common";
+		Icon = "rbxassetid://96329947106909";
+	};
+	["Ember Berries"] = {
+		LureSpeed = 5;
+		Luck = 120;
+		GenerelLuck = 45;
+		Resilience = 15;
+		Rarity = "Unusual";
+		Icon = "rbxassetid://106591280919281";
+	};
+	["Crystal Bananas"] = {
+		LureSpeed = 0;
+		Luck = 150;
+		GenerelLuck = 90;
+		Resilience = 25;
+		Rarity = "Rare";
+		Icon = "rbxassetid://132112236254457";
+	};
+	["Mist Worms"] = {
+		LureSpeed = 20;
+		Luck = 100;
+		GenerelLuck = 80;
+		Resilience = 25;
+		Rarity = "Legendary";
+		Icon = "rbxassetid://75993334361705";
+	};
+	["Lagoon Leech"] = {
+		LureSpeed = 15;
+		Luck = 75;
+		GenerelLuck = -10;
+		Resilience = 20;
+		Rarity = "Rare";
+		Icon = "rbxassetid://131228054836930";
+	};
+	["Sapphire Krill"] = {
+		LureSpeed = 5;
+		Luck = 150;
+		GenerelLuck = 150;
+		Resilience = 40;
+		Rarity = "Mythical";
+		Icon = "rbxassetid://105153897307029";
+	};
+	["Gale Grub"] = {
+		LureSpeed = 30;
+		Luck = 85;
+		GenerelLuck = -10;
+		Resilience = -5;
+		Rarity = "Common";
+		Icon = "rbxassetid://89278213759164";
+	};
+	["Luminous Larva"] = {
+		LureSpeed = 10;
+		Luck = 200;
+		GenerelLuck = 175;
+		Resilience = 50;
+		Rarity = "Exotic";
+		Icon = "rbxassetid://98270395522373";
+	};
+	["Stud Bait"] = {
+		Icon = "rbxassetid://92827044886722";
+		LureSpeed = 50;
+		Luck = 150;
+		GenerelLuck = 40;
+		Resilience = 20;
+		Rarity = "Legendary";
+		Limited = true;
+	};
+	["Colossal Ink Bait"] = {
+		Icon = "rbxassetid://78817927104320";
+		LureSpeed = 5;
+		Luck = 150;
+		GenerelLuck = 150;
+		Resilience = 40;
+		Rarity = "Mythical";
+		Limited = true;
+		Mutation = "Colossal Ink";
+		WeightBoost = 2;
+	};
+	["Hourglass Bait"] = {
+		Icon = "rbxassetid://136590369641722";
+		LureSpeed = 5;
+		Luck = 150;
+		GenerelLuck = 150;
+		Resilience = 40;
+		XpBoost = 1;
+		Rarity = "Mythical";
+		Limited = true;
+		Mutation = "Neon";
+		WeightBoost = 2;
+	};
+	["Golden Shrimp Bait"] = {
+		Icon = "rbxassetid://101398702799761";
+		LureSpeed = 15;
+		Luck = 120;
+		GenerelLuck = 90;
+		Resilience = 10;
+		Price = 1500;
+		Rarity = "Mythical";
+	};
+	["Whale Bait"] = {
+		Icon = "rbxassetid://76695597473114";
+		LureSpeed = -5;
+		Luck = 180;
+		GenerelLuck = 20;
+		Resilience = 30;
+		Price = 800;
+		Rarity = "Legendary";
+	};
